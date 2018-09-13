119269fe300128a61a73d753e188dfa703da1aee
Ekkachai <Ekkachai.thong@silkspan.local>
2018-09-13 16:18:55 +0700

    login : add connect api function

--- src/_Service/user.service.js
+++ src/_Service/user.service.js
@@ -1,19 +1,20 @@
 import axios from 'axios'
 // import config from './../config'
-// axios.defaults.headers = {
-//   'contentType': "application/json; charset=utf-8",
-// }
+axios.defaults.headers = {
+  'contentType': "application/json; charset=utf-8",
+}
 export const userService = {
   login,
   logout
 }
 
 async function login(userName, passWord) {
-  const { data } = await axios.post("http://192.168.0.142:5000/api/login", { userName, passWord }, {
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
+  try {
+    const { data } = await axios.post("http://localhost:5000/api/login", { userName, passWord });
+    console.log('e :', data);
+  } catch (error) {
+    console.log('e :', e);
+  }
   
   return data;
 }
